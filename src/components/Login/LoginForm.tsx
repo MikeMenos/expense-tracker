012ccffaa474c14e1e52f5db4695183e71977732
mmenos <m.menos@protera.com>
2023-01-12 18:20:26 +0200

    Added Log In Button

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -2,23 +2,19 @@ import Input from "../shared/Input";
 import { type FC, type SyntheticEvent } from "react";
 
 interface PropsInterface {
-  username: string;
+  email: string;
   password: string;
   onInputChange: (e: SyntheticEvent) => void;
 }
 
-const LoginForm: FC<PropsInterface> = ({
-  username,
-  password,
-  onInputChange,
-}) => {
+const LoginForm: FC<PropsInterface> = ({ email, password, onInputChange }) => {
   return (
     <div className="flex flex-col gap-6">
       <Input
         type="email"
-        name="username"
+        name="email"
         placeholder="Email"
-        value={username}
+        value={email}
         onChange={onInputChange}
         className="w-full rounded-md bg-main p-2 outline-none"
       />
